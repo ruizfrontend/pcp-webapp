4afb519c108d9499315d0f9a843eaf220ac5551e
DavidR <ruizfrontend@gmail.com>
2016-05-06 05:41:50 +0000

    ajuste para cloud9

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -6,6 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+    
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
