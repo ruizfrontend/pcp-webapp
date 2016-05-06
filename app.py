5bcbb3f42b3966b60d4af01bef9de025e52b9a1a
DavidR <ruizfrontend@gmail.com>
2016-05-06 06:04:07 +0000

    montando página de detalle

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ app = Flask(__name__)
 csv_path = './static/incendio.csv'
 csv_obj = csv.DictReader(open(csv_path, 'r'))
 csv_list = list(csv_obj)
+csv_dict = dict([[o['IDPIF'], o] for o in csv_list])
 
 @app.route("/")
 def index():
     return render_template('index.html',
         object_list=csv_list,
+    )
+@app.route('/<number>/')
+def detail(number):
+    return render_template('detail.html',
+        object=csv_dict[number],
     )
 
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
