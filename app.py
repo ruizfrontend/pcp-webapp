3a9e7b188fe3218cdfa57f3bfc0aec10a2986481
DavidR <ruizfrontend@gmail.com>
2016-05-06 04:54:12 +0000

    configurando servidor

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        use_reloader=True,
+        debug=True,
+    )
