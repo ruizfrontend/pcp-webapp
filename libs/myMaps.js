530c8c3b33a753dfff273c49aec127e38dc23b12
DavidR <ruizfrontend@gmail.com>
2016-05-21 16:55:31 +0000

    un par de ajustes mas

--- libs/myMaps.js
+++ libs/myMaps.js
@@ -116,7 +116,8 @@ var loadFullmap = function(){
 var mapInit = function(id, position, zoom) {
   	  var map = L.map(id).setView(position, zoom);
       var mapquestLayer = new L.TileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-          maxZoom: 18,
+          maxZoom: 12,
+          minZoom: 5,
           attribution: 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>,<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors.',
           subdomains: ['otile1','otile2','otile3','otile4']
       });
